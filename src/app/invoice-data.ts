cd6ac65df0aba33814602521a1d338dab70818a2
abhaysahu <42239059+abhaysahu@users.noreply.github.com>
2019-04-12 00:53:21 +0530

    MysecondCommit

---
+++ src/app/invoice-data.ts
@@ -0,0 +1,11 @@
+import { InvoiceRow } from './invoice-row';
+
+export const invoiceData = [
+  new InvoiceRow('BANANA', 29, 5),
+  new InvoiceRow('APPLE', 39, 7),
+  new InvoiceRow('WATERMELON', 15, 5),
+  new InvoiceRow('ORANGES', 25, 2),
+  new InvoiceRow('PEAR', 35, 2),
+  new InvoiceRow('CHERRIES', 16, 5)
+];
+
